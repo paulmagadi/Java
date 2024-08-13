0f72a69cd1c732d5cbe6d2846e8814e9e4feb773
SQCodes <paulsqmagadi@gmail.com>
2024-08-13 15:38:50 +0300

    new file:   .vscode/settings.json 	new file:   Main.class 	new file:   Main.java

---
+++ Main.java
@@ -0,0 +1,83 @@
+/* public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World! This is my first Java code.");
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("I am learning Java.");
+    System.out.println("It is awesome!");
+  }
+} 
+*/
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       // Student data
+//       String studentName = "John Doe";
+//       int studentID = 15;
+//       int studentAge = 23;
+//       float studentFee = 75.25f;
+//       char studentGrade = 'B';
+      
+//       // Print variables
+//       System.out.println("Student name: " + studentName);
+//       System.out.println("Student id: " + studentID);
+//       System.out.println("Student age: " + studentAge);
+//       System.out.println("Student fee: " + studentFee);
+//       System.out.println("Student grade: " + studentGrade);
+//     }
+// }
+
+
+// // Calculate the area of a rectangle
+// public class Main {
+//     public static void main(String[] args) {
+//       // Create integer variables
+//       int length = 4;
+//       int width = 6;
+//       int area;
+  
+//       // Calculate the area of a rectangle
+//       area = length * width;
+  
+//       // Print variables
+//       System.out.println("Length is: " + length);
+//       System.out.println("Width is: " + width);
+//       System.out.println("Area of the rectangle is: " + area);
+//     }
+//   }
+  
+
+// public class Main {
+//     public static void main(String[] args) {
+//       // Create variables of different data types
+//       int items = 50;
+//       float costPerItem = 9.99f;
+//       float totalCost = items * costPerItem;
+//       char currency = '$';
+  
+//       // Print variables
+//       System.out.println("Number of items: " + items);
+//       System.out.println("Cost per item: " + costPerItem + currency);
+//       System.out.println("Total cost = " + totalCost + currency);
+//     }
+//   }
+
+
+// Widening Casting
+// Widening casting is done automatically when passing a smaller size type to a larger size type:
+
+// ExampleGet your own Java Server
+// public class Main {
+//   public static void main(String[] args) {
+//     int myInt = 9;
+//     double myDouble = myInt; // Automatic casting: int to double
+
+//     System.out.println(myInt);      // Outputs 9
+//     System.out.println(myDouble);   // Outputs 9.0
+//   }
+// }
