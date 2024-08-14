7f4169d47f2c1935ab25717328d6dd95b315c61e
SQCodes <paulsqmagadi@gmail.com>
2024-08-14 13:36:15 +0300

    deleted:    Main.class 	modified:   Main.java

--- Main.java
+++ Main.java
@@ -105,30 +105,43 @@ public class Main {
 //     }
 //   }
 
-public class Main {
-    public static void main(String[] args) {
-      // An array storing different ages
-      int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+// public class Main {
+//     public static void main(String[] args) {
+//       // An array storing different ages
+//       int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
   
-      float avg, sum = 0;
+//       float avg, sum = 0;
   
-      // Get the length of the array
-      int length = ages.length;
+//       // Get the length of the array
+//       int length = ages.length;
       
-      // Create a 'lowest age' variable and assign the first array element of ages to it
-      int lowestAge = ages[0];
+//       // Create a 'lowest age' variable and assign the first array element of ages to it
+//       int lowestAge = ages[0];
   
-      // Loop through the elements of the ages array to find the lowest age
-      for (int age : ages) {
-        // Check if the current age is smaller than the current 'lowest age'
-        if (lowestAge > age) {
-          // If the smaller age is found, update 'lowest age' with that element
-          lowestAge = age;
-        }
-      }
+//       // Loop through the elements of the ages array to find the lowest age
+//       for (int age : ages) {
+//         // Check if the current age is smaller than the current 'lowest age'
+//         if (lowestAge > age) {
+//           // If the smaller age is found, update 'lowest age' with that element
+//           lowestAge = age;
+//         }
+//       }
   
-      // Output the value of the lowest age
-      System.out.println("The lowest age in the array is: " + lowestAge);
+//       // Output the value of the lowest age
+//       System.out.println("The lowest age in the array is: " + lowestAge);
+//     }
+// }
+
+public class Main {
+  public static void main(String[] args) {
+    int result = sum(10);
+    System.out.println(result);
+  }
+  public static int sum(int k) {
+    if (k > 0) {
+      return k + sum(k - 1);
+    } else {
+      return 0;
     }
   }
-  
+}
